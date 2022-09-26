5bc7d1c38314231728ed6b91fc219704c559686e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-26 07:28:10 +0530

    ADD:CONDITION TO CHECK VALID MAIL ADDRESS

--- Regex.java
+++ Regex.java
@@ -5,13 +5,15 @@ import java.util.regex.Pattern;
 
 public class Regex {
 	public static void main(String[] args) {
-		Pattern firstName = Pattern.compile("^[A-Z][a-z]{3,}");//first name start with capital and has minimum 3 chararcters
+		Pattern firstName = Pattern.compile("^[A-Z][a-z]{3,}");
 		Matcher first = firstName.matcher("Varunkumar");
 		Pattern lastName = Pattern.compile("^[A-Z][a-z]{3,}");
 		Matcher last = lastName.matcher("Venkatesan");
+		Pattern email = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+		Matcher mail = email.matcher("abc@gmail.com");
 
-		System.out.println("string matches the given Regex -"+first.matches());
-		System.out.println("string matches the given Regex -"+last.matches());
-}
+		System.out.println("string matches the given Regex -" + first.matches());
+		System.out.println("string matches the given Regex -" + last.matches());
+		System.out.println("string matches the given Regex -" + mail.matches());
 	}
-
+}
