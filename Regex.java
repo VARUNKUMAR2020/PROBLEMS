c5f382b1d5a9f065e1da88f16827f7e6595d1ac3
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-26 07:11:23 +0530

    ADD:CONDITION GIVEN TO THE NAME

--- Regex.java
+++ Regex.java
@@ -1,8 +1,13 @@
 package Regex.Regex;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Regex 
 {
     public static void main( String[] args )
     {
         System.out.println( "WELCOME TO REGEX BASED PROGRAM " );
+        Pattern CONDITION = Pattern.compile("^[A-Z][a-z]{3,}");
+		Matcher CHECK = CONDITION.matcher("Varunkumar");
+		System.out.println("CONDITTION OF THE NAME GIVEN -"+CHECK.matches());
     }
 }
