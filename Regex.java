4e6ed97217e6c5949f9dce8e4c5f2663dc3b87ed
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-26 07:32:34 +0530

    ADD:CONDITION TO CHECK VALID MOBILE NUMBER

--- Regex.java
+++ Regex.java
@@ -11,9 +11,12 @@ public class Regex {
 		Matcher last = lastName.matcher("Venkatesan");
 		Pattern email = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
 		Matcher mail = email.matcher("abc@gmail.com");
+		Pattern Mobile = Pattern.compile("[0-9]{0,2}[1-9] [0-9]{10}");
+		Matcher mobile = Mobile.matcher("91 8976544391");
 
 		System.out.println("string matches the given Regex -" + first.matches());
 		System.out.println("string matches the given Regex -" + last.matches());
 		System.out.println("string matches the given Regex -" + mail.matches());
+		System.out.println("string matches the given Regex -" + mobile.matches());
 	}
 }
